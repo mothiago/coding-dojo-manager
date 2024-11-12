d4165a03ef28dcb9b1a498c90341fcb636d6c122
Thiago Oliveira <thiago.oliveira@agilize.com.br>
2024-11-12 16:46:45 -0300

    Corrige deploy

--- next.config.js
+++ next.config.js
@@ -1,12 +1,22 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/coding-dojo-manager' : '',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
     unoptimized: true,
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/coding-dojo-manager/' : '',
-  trailingSlash: true,
 }
 
 module.exports = nextConfig 
