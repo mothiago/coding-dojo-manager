cac33bf88e9e0db31f806321c34d91086352bf2b
Thiago Oliveira <thiago.oliveira@agilize.com.br>
2024-11-12 16:31:18 -0300

    Corrige deploy

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/coding-dojo-manager',
+  basePath: process.env.NODE_ENV === 'production' ? '/coding-dojo-manager' : '',
   images: {
     unoptimized: true,
   },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/coding-dojo-manager/' : '',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig 
